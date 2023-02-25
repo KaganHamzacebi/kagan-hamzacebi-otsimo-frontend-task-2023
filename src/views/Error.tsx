cd32d0c3f3ee70376aad162bc5781eca1e503c7f
KaganHamzacebi <kagan.hcebi@gmail.com>
2023-02-25 12:18:54 +0300

    error page implemented

--- src/views/Error.tsx
+++ src/views/Error.tsx
@@ -1,7 +1,17 @@
+import styles from '../styles/modules/Error.module.scss';
+import ErrorLogo from '../assets/error.png';
+import { useNavigate } from 'react-router-dom';
+
 function Error() {
+  const navigate = useNavigate();
 
   return (
     <>
+      <main className={styles.main}>
+        <img onClick={() => navigate('/')} src={ErrorLogo} className={styles.errorLogo} alt='error_logo' />
+        <h1 className={styles.errorMessage}>Sorry the page you looking for does not exist!</h1>
+        <span className={styles.returnMessage}>Turn back to home page by clicking the image</span>
+      </main>
     </>
   );
 }
